02ab0428e120c508f4f61089301821147c873ef8
MarcoPorcaro1987 <marcoporcaro87@gmail.com>
2021-09-21 09:32:40 +0100

    Refactoring

--- js/main.js
+++ js/main.js
@@ -2,21 +2,32 @@ var app = new Vue({
     el: '#root',
     data: {
         todos : [
-            "Fare i compiti",
-            "Fare la spesa",
-            "Fare il bucato"
+            {
+                compito: "Fare i compiti",
+                fatto: false
+            },
+            {
+                compito: "Fare la spesa",
+                fatto: false    
+            },
+            {
+                compito: "Fare il bucato",
+                fatto: false
+            }
         ],
         newTodo : ""
     },
     methods: {
         addTodo : function(){
             if(this.newTodo != ""){
-            this.todos.push(this.newTodo);
-            this.newTodo = ""
+                this.todos.push({
+                    compito : this.newTodo, fatto : false
+                });
+                this.newTodo = ""
             }
         },
         removeTodo: function(index){
             this.todos.splice(index, 1);
-        }
+        },
     }
   });
